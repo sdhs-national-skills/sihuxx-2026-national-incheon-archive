f22ae19e7dc5de4aacd72a47410307d00b06d13a
sihuxx <sihu714@gmail.com>
2026-06-22 20:57:52 +0900

    DB까먹음

--- incheonarchive.sql
+++ incheonarchive.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 26-06-22 10:10
+-- 생성 시간: 26-06-22 13:57
 -- 서버 버전: 10.4.32-MariaDB
 -- PHP 버전: 8.2.12
 
@@ -40,7 +40,50 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`idx`, `user_idx`, `post_idx`, `date`, `content`) VALUES
-(1, 1, 1, '2026-06-21 06:11:59', '따뜻한 댓글');
+(4, 2, 3, '2026-06-22 08:56:21', 'zz');
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `comments_likes`
+--
+
+CREATE TABLE `comments_likes` (
+  `idx` int(11) NOT NULL,
+  `comment_idx` int(11) NOT NULL,
+  `user_idx` int(11) NOT NULL,
+  `post_idx` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 테이블의 덤프 데이터 `comments_likes`
+--
+
+INSERT INTO `comments_likes` (`idx`, `comment_idx`, `user_idx`, `post_idx`, `date`) VALUES
+(27, 0, 2, 0, '2026-06-22 09:28:52'),
+(30, 4, 2, 3, '2026-06-22 09:33:03');
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `debates`
+--
+
+CREATE TABLE `debates` (
+  `idx` int(11) NOT NULL,
+  `title` varchar(400) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `result` int(11) NOT NULL,
+  `user_idx` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 테이블의 덤프 데이터 `debates`
+--
+
+INSERT INTO `debates` (`idx`, `title`, `date`, `result`, `user_idx`) VALUES
+(1, '엉덩이는 두개인가 하나인가', '2026-06-22 11:49:45', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -60,7 +103,8 @@ CREATE TABLE `likes` (
 --
 
 INSERT INTO `likes` (`idx`, `user_idx`, `post_idx`, `date`) VALUES
-(15, 1, 1, '2026-06-21 06:10:04');
+(26, 2, 0, '2026-06-22 09:33:00'),
+(27, 2, 3, '2026-06-22 09:33:02');
 
 -- --------------------------------------------------------
 
@@ -83,7 +127,6 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`idx`, `title`, `category`, `date`, `detail`, `photo`, `user_idx`) VALUES
-(1, 'test 게시글', '공부', '2026-06-21', '게시글입니다.', '', 1),
 (2, '게시글입니다', '자유', '2026-06-22', '배가 너무 고프다', '', 1),
 (3, '이미지 테스트', '자유', '2026-06-22', '이미지', '/asset/posts/꺅두기.png,/asset/posts/두기.jpg', 1);
 
@@ -109,7 +152,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`idx`, `id`, `pw`, `name`, `profile`, `is_admin`, `login_token`) VALUES
 (1, 'sihu', '1234', '시후', '/asset/profile/꺅두기.png', 0, NULL),
-(2, 'admin', '1234', '관리자', '/asset/profile/귀여운 두기.tmp', 1, '1604a261aeb4ec4eeb0eae10545302e7a56884d57271f1c4a0547a4b8ef514ad');
+(2, 'admin', '1234', '관리자', '/asset/profile/귀여운 두기.tmp', 1, '795b79b25bec1684595d5fa86a8f3f3c71ce234281c8de43d2eb585b44510eb5');
 
 --
 -- 덤프된 테이블의 인덱스
@@ -119,6 +162,18 @@ INSERT INTO `users` (`idx`, `id`, `pw`, `name`, `profile`, `is_admin`, `login_to
 -- 테이블의 인덱스 `comments`
 --
 ALTER TABLE `comments`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- 테이블의 인덱스 `comments_likes`
+--
+ALTER TABLE `comments_likes`
+  ADD PRIMARY KEY (`idx`);
+
+--
+-- 테이블의 인덱스 `debates`
+--
+ALTER TABLE `debates`
   ADD PRIMARY KEY (`idx`);
 
 --
@@ -147,13 +202,25 @@ ALTER TABLE `users`
 -- 테이블의 AUTO_INCREMENT `comments`
 --
 ALTER TABLE `comments`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 테이블의 AUTO_INCREMENT `comments_likes`
+--
+ALTER TABLE `comments_likes`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- 테이블의 AUTO_INCREMENT `debates`
+--
+ALTER TABLE `debates`
   MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 테이블의 AUTO_INCREMENT `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- 테이블의 AUTO_INCREMENT `posts`
